308d78e4cc371f3cfbd575ae1a0062c22edeceb4
Neha Batra <nehalita@gmail.com>
2013-05-03 21:28:10 -0400

    Simplified variables passed into templates to be locals().

--- bloggo/blog/views.py
+++ bloggo/blog/views.py
@@ -8,19 +8,16 @@ from models import Entry
 def hello_world(request):
     return HttpResponse("Hello world!")
 
-def froogle(request):
-    return HttpResponse("Blah!")
-
 def entry_list(request):
     entries = Entry.objects.all()
-    return render(request, "entry_list.html", {"entries": entries})
+    return render(request, "entry_list.html", locals())
 
 def entry_detail(request, entry_id):
     try:
         entry = Entry.objects.get(pk=entry_id)
     except Entry.DoesNotExist:
         return HttpResponse (status=404)
-    return render(request, "entry_detail.html", {"entry": entry})
+    return render(request, "entry_detail.html", locals())
 
 # creates a link to blog post number 51 on handler named entry_detail
 # reverse("entry_detail", 51)
