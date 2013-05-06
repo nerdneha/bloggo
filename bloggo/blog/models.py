7e6d916c758af097aa7c8ecea9b148734fefee78
Neha Batra <nehalita@gmail.com>
2013-05-06 13:14:02 -0400

    Fixed link error - mostly dealing with commenting strings.

--- bloggo/blog/models.py
+++ bloggo/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import permalink
 from django.core.urlresolvers import reverse
 
 # Create your models here.
@@ -8,5 +9,7 @@ class Entry(models.Model):
     post_date = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=225) #, unique=True)
 
+    #@permalink
     def get_absolute_url(self):
         return reverse('bloggo.blog.views.view_entry', args=[str(self.id)])
+
--- bloggo/blog/views.py
+++ bloggo/blog/views.py
@@ -18,9 +18,13 @@ def entry_list(request):
     return render(request, "entry_list.html", locals())
 
 def view_entry(request, entry_id):
+    print "we got here"
     entry = get_object_or_404(Entry,pk=entry_id)
     return render(request, "entry_detail.html", locals())
-
+'''
+def add_entry(request):
+    return render(request, "
+'''
 # creates a link to blog post number 51 on handler named entry_detail
 # reverse("entry_detail", 51)
 # "http://mysite.com/entry/51"
